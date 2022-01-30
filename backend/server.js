9254445ad0b880fba770df203ac5f01e76b2c615
DevEnock <enockomondi305@gmail.com>
2022-01-30 12:33:52 +0300

    adding database url

--- backend/server.js
+++ backend/server.js
@@ -1,14 +1,15 @@
-import express from 'express';
-import mongoose from 'mongoose';
-import cors from 'cors';
+const express = require('express');
+const mongoose = require('mongoose');
+const cors = require('cors');
 
 // importing routes
 const patientRoute = require('./routes/patient');
 const doctorRoute = require('./routes/doctor'); 
 
-
 const app = express();
 const PORT = process.env.PORT || 4000;
+
+require('dotenv').config();
 
 // add middleware
 app.use(cors());
@@ -17,6 +18,16 @@ app.use(express.json());
 // add routes as middleware
 app.use('/patients', patientRoute);
 app.use('/doctors', doctorRoute);
+
+// connecting to the database
+const url = process.env.DATABASE_URL;
+mongoose.connect(url);
+
+const connection = mongoose.connection;
+
+connection.once('open', () =>{
+  console.log('Application is connected to the database successfully!');
+})
 
 
 app.listen(PORT, () =>{
