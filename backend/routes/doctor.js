13f3fce978e73c40cb585a0ff3958bc61deb02db
DevEnock <enockomondi305@gmail.com>
2022-02-25 12:24:34 +0300

    adding doctors route

--- backend/routes/doctor.js
+++ backend/routes/doctor.js
@@ -1,6 +1,15 @@
-const router = require('express').Router();
+const express = require('express')
+const router = express.Router();
 
 const Doctor = require('../models/doctor.model');
+
+router.route('/').get((req, res) =>{
+    Doctor.find()
+          .then(doctors => res.json(doctors))
+          .catch((error) => res.status(400).json('Error ' + error));
+});
+
+module.exports = router;
 
 
 
