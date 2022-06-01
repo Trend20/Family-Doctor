3a2d09a1455f0c8b2fe518054bb727d535612a93
Enock Omondi <enockomondi305@2019>
2022-06-01 12:02:17 +0300

    fixing contact orm errors

--- src/components/Contact/Contact.js
+++ src/components/Contact/Contact.js
@@ -2,7 +2,7 @@ import React, { Component } from 'react';
 
 import { RiPhoneFill, RiMap2Line } from 'react-icons/ri';
 import { MdEmail } from 'react-icons/md';
-import Swal from "sweetalert2"; 
+import Swal from "sweetalert2";
 import axios from 'axios';
 
 import './Contact.css';
@@ -25,101 +25,125 @@ class Contact extends Component {
 	}
 
 	// name handler
-	handleNameChange = (event) =>{
+	handleNameChange = (event) => {
 		this.setState({
 			name: event.target.value
 		})
 	}
 
 	// email handler
-	handleEmailChange = (event) =>{
+	handleEmailChange = (event) => {
 		this.setState({
 			email: event.target.value
 		})
 	}
 	// phone handler
-	handlePhoneChange = (event) =>{
+	handlePhoneChange = (event) => {
 		this.setState({
 			phone: event.target.value
 		})
 	}
 
 	// message handler
-	handleMessageChange = (event) =>{
+	handleMessageChange = (event) => {
 		this.setState({
 			message: event.target.value
 		})
 	}
 
 
-	validate = () =>{
-		let nameError = "";
-    let emailError = "";
-    let phoneError = "";
-		let messageError = ""
 
-		if(!this.state.name){
-			nameError = "Name field is required";
-		}else{
-			nameError = ""
+	validate = () => {
+
+		if (this.state.name === "") {
+			this.setState({
+				nameError: "Name field is required"
+			})
+		} else {
+			this.setState({
+				nameError: ""
+			})
 		}
 
 		const reg = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/;
-    if (!this.state.email || reg.test(this.state.email) === false) {
-      emailError = "Email Field is Invalid ";
-    }
+		if (this.state.email === "" || reg.test(this.state.email) === false) {
+			this.setState({
+				emailError: "Email Field is Invalid"
+			})
+		} else {
+			this.setState({
+				emailError: ""
+			})
+		}
 
-    if (!this.state.phone) {
-      phoneError = "Phone field is required";
-    }
+		if (this.state.phone === "") {
+			this.setState({
+				phoneError: "Phone field is required"
+			})
+		} else {
+			this.setState({
+				phoneError: ""
+			})
+		}
 
-		if (!this.state.message) {
-      messageError = "Message field is required";
-    }
-
-    if (emailError || nameError || phoneError || messageError) {
-      this.setState({ nameError, emailError, phoneError, messageError });
-     return false;
-    }
-    
-			Swal.fire({ 
-				icon: 'success', 
-				title: 'Success',  
-				type: 'success',  
-				text: 'We will get back to you shortly!',  
-			});  
+		if (this.state.message === "") {
+			this.setState({
+				messageError: "Message field is required"
+			})
+		} else {
+			this.setState({
+				messageError: ""
+			})
+		}
 	}
 
-
-
-
+	showSuccess = () =>{
+		if(this.state.name !== "" && this.state.email !== "" && this.state.phone !== "" && this.state.message !== ""){
+			Swal.fire({
+				icon: 'success',
+				title: 'Welcome',
+				text: 'We will get back to you shortly!',
+				timer: 2500
+			})
+		}
+	}
 
 	// handling form submit
-	onFormSubmit = (event) =>{
+	onFormSubmit = (event) => {
 
-      event.preventDefault();
+		event.preventDefault();
 
-			const newUser ={
-				name: this.state.name,
-				email: this.state.email,
-				phone: this.state.phone,
-				message: this.state.message
-			}
+		const newUser = {
+			name: this.state.name,
+			email: this.state.email,
+			phone: this.state.phone,
+			message: this.state.message
+		}
 
-			axios.post('http:localhost:4000/patients/add', newUser)
+		axios.post('http:localhost:4000/patients/add', newUser)
 			.then(res => {
 				console.log(res.data);
 			});
 
-			this.validate();
+		this.validate();
+		this.showSuccess();
 
-			this.setState( state =>({
+		if(this.state.name !== "" && this.state.email !== "" && this.state.phone !== "" && this.state.message !== ""){
+			this.setState(state => ({
 				users: state.users.concat(newUser),
 				name: '',
 				email: '',
 				phone: '',
 				message: ''
 			}))
+		}else{
+			Swal.fire({
+				icon: 'error',
+				title: 'Oops...',
+				text: 'Please fill all the fields!',
+				timer: 2500
+			})
+		}
 	}
 
 	render() {
@@ -167,31 +191,31 @@ class Contact extends Component {
 						<form onSubmit={this.onFormSubmit}>
 							<h1>Get In Touch</h1>
 
-							<input type="text" placeholder="Name*" 
-							value={this.state.name} 
-							className="input-field"
-							onChange={this.handleNameChange} 
+							<input type="text" placeholder="Name*"
+								value={this.state.name}
+								className="input-field"
+								onChange={this.handleNameChange}
 							/>
 							<span className="text-danger">{this.state.nameError}</span>
 
-							<input type="email" placeholder="Email*" 
-							value={this.state.email} 
-							className="input-field" 
-							onChange={this.handleEmailChange} 
+							<input type="email" placeholder="Email*"
+								value={this.state.email}
+								className="input-field"
+								onChange={this.handleEmailChange}
 							/>
 							<span className="text-danger">{this.state.emailError}</span>
 
-							<input type="tel" placeholder="Phone" 
-							value={this.state.phone} 
-							className="input-field" 
-							onChange={this.handlePhoneChange} 
+							<input type="tel" placeholder="Phone"
+								value={this.state.phone}
+								className="input-field"
+								onChange={this.handlePhoneChange}
 							/>
 							<span className="text-danger">{this.state.phoneError}</span>
 
-							<textarea name="message" id="message" 
-							value={this.state.message} 
-							cols="30" rows="2" placeholder="Message*"
-							onChange={this.handleMessageChange} 
+							<textarea name="message" id="message"
+								value={this.state.message}
+								cols="30" rows="2" placeholder="Message*"
+								onChange={this.handleMessageChange}
 							></textarea>
 							<span className="text-danger">{this.state.messageError}</span>
 
